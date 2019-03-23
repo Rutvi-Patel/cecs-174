187a1bf52e0e92cde8f4b519467a6a97ae0be370
Rutvi Patel <47683691+Rutvi-Patel@users.noreply.github.com>
2019-03-23 16:34:28 -0700

    Add files via upload

---
+++ simple_function.py
@@ -0,0 +1,15 @@
+def smallest(x,y,z) :
+    list = [x,y,z]
+    mini = min(list)
+    return (mini)
+def average(x,y,z):
+    avg = (x+y+z)/3
+    return avg
+print(average(10,20,30))
+
+print(smallest(10,20,30))
+
+print(smallest(17,5,11))
+
+print(smallest(23,27,4))
+
